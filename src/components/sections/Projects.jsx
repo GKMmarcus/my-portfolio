45a8a92b94333f1ae144bb8a184060ff21aa622b
Marcus <marcusphan1234@gmail.com>
2025-12-11 14:38:29 -0600

    Added more content in the project sections

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -7,6 +7,21 @@ export const Projects = () => {
                 </h2>
                 <div className ="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className = "p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-red-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                        <h3 className ="text-xl font-bold mb-2">Sports Sales Dashboard</h3>
+                        <p className ="text-gray-400 mb-3">A data dashboard that shows sportwear trends from 2023 to help optimize business decsions for 2024</p>
+                        <div>
+                            {["PHP","MariaDB","HTML","CSS"].map((tech,key) => (
+                                <span key = {key} className = "bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                        {tech}
+                                </span>
+                            ))}
+                        </div>
+                        <div className = "flex justify-between items-center">
+                            <a href = "https://public.tableau.com/app/profile/marcus.phanphagna/viz/shared/86Q5KN25H" className = "text-red-400 hover:text-red-300 transition-colors"> View Project </a>
+                        </div>
+                    </div>
+
+                    <div className = "p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-red-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
                         <h3 className ="text-xl font-bold mb-2">Stress Management and Anxiety App</h3>
                         <p className ="text-gray-400 mb-3"> A full-stack app that guides users through breathing, meditation, and journaling to reduce anxiety and track progress.</p>
                         <div>
